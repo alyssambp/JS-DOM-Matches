8460af61dc8320545be70d5b879b2ef3b8fa892a
Alyssa Pagulayan <99alyssapagulayan@gmail.com>
2023-05-15 15:36:38 +0100

    Cards are now displayed

---
+++ scripts.js
@@ -0,0 +1,43 @@
+var cardsArray = [
+    {    'name': 'CSS',    'img': 'https://github.com/robgmerrill/img/blob/master/css3-logo.png?raw=true',  },
+    {    'name': 'HTML',    'img': 'https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true',  },
+    {    'name': 'jQuery',    'img': 'https://github.com/robgmerrill/img/blob/master/jquery-logo.png?raw=true',  },
+    {    'name': 'JS',    'img': 'https://github.com/robgmerrill/img/blob/master/js-logo.png?raw=true',  },
+    {    'name': 'Node',    'img': 'https://github.com/robgmerrill/img/blob/master/nodejs-logo.png?raw=true',  },
+    {    'name': 'Photo Shop',    'img': 'https://github.com/robgmerrill/img/blob/master/photoshop-logo.png?raw=true',  },
+    {    'name': 'PHP',    'img': 'https://github.com/robgmerrill/img/blob/master/php-logo_1.png?raw=true',  },
+    {    'name': 'Python',    'img': 'https://github.com/robgmerrill/img/blob/master/python-logo.png?raw=true',  },
+    {    'name': 'Ruby',    'img': 'https://github.com/robgmerrill/img/blob/master/rails-logo.png?raw=true',  },
+    {    'name': 'Sass',    'img': 'https://github.com/robgmerrill/img/blob/master/sass-logo.png?raw=true',  },
+    {    'name': 'Sublime',    'img': 'https://github.com/robgmerrill/img/blob/master/sublime-logo.png?raw=true',  },
+    {    'name': 'Wordpress',    'img': 'https://github.com/robgmerrill/img/blob/master/wordpress-logo.png?raw=true',  },
+];
+
+// Grab the div with an id of game-board and assign to a variable game
+var game = document.getElementById("game-board");
+// Create a section element and assign it to variable grid
+var grid = document.createElement("section");
+// Give section element a class of grid
+grid.setAttribute("class", "grid");
+// Append the grid section to the game-board div
+game.appendChild(grid);
+
+// Loop through each item in our cards array
+for (i = 0; i < cardsArray.length; i++) {
+
+    // Create a div element and assign to variable card
+    var card = document.createElement("div");
+
+    // Apply a card class to that div
+    card.classList.add("card");
+
+    // Set the data-name attribute of the div to the cardsArray name
+    card.dataset.name = cardsArray[i].name;
+
+    // Apply the background image of the div to the cardsArray image
+    card.style.backgroundImage = `url(${cardsArray[i].img})`; // use the ` symbol
+
+    // Append the div to the grid section
+    grid.appendChild(card);
+
+};
